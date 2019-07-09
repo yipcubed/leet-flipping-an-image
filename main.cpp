0cb92d112e25d19be555ab6202ff52149856aed4
Yip Cubed <yipcubed@gmail.com>
2019-07-08 14:05:42 -1000

    starting

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <cstring>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <sstream>
+
+
+#include "../utils/VectorUtils.hpp"
+#include "../utils/PrintUtils.hpp"
+
+// https://leetcode.com/problems/flipping-an-image/
+
+// #include "../utils/HeapUtils.hpp"
+// #include "../utils/BinarySearch.hpp"
+// #include "../utils/TreeUtils.hpp"
+
+using namespace std;
+
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+
+// Live coding problems, watch at
+// https://www.twitch.tv/yipcubed
+// https://www.youtube.com/channel/UCTV_UOPu7EWXvYWsBFxMsSA/videos
+//
+
+// makes code faster, but larger. Just for LeetCode fun!
+#pragma GCC optimise ("Ofast")
+
+// makes stdin not synced so it is faster. Just for LeetCode fun!
+static auto __ __attribute__((unused)) = []() {              // NOLINT
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        for (auto& v : A) {
+            reverse(v.begin(), v.end());
+        }
+        for (auto& v : A) {
+            for (int i = 0; i < v.size(); ++i) {
+                v[i] = v[i] == 0 ? 1 : 0;
+            }
+        }
+        return A;
+    }
+};
+
+void test1() {
+    vector<vector<int>> v1{
+            vector<int>{1, 1, 0},
+            vector<int>{1, 0, 1},
+            vector<int>{0, 0, 0}
+    };
+
+    cout << " ? " << Solution().flipAndInvertImage(v1) << endl;
+}
+
+void test2() {
+
+}
+
+void test3() {
+
+}
